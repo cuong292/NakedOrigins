5b76083a720a9dc6f8c2602a4bc8f092a8814007
cuongnb <cuongnb@gemvietnam.com>
2019-05-04 18:02:44 +0700

    first commit

---
+++ GemCore/src/main/java/com/gemvietnam/Constants.java
@@ -0,0 +1,12 @@
+package com.gemvietnam;
+
+/**
+ * Common constants
+ * Created by neo on 7/18/2016.
+ */
+public interface Constants {
+  String EMPTY_STRING = "";
+  String GOOGLE_MAP_GEOCODE_URL = "http://maps.google.com/maps/api/geocode/";
+  String GOOGLE_API_URL = "https://www.googleapis.com/";
+  String OAUTH2_API_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/";
+}
